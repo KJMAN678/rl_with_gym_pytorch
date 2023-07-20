3b464e316ba519a28b504b028c7b9d70e30c76fe
KJMAN678 <kj.fireman5@gmail.com>
2023-07-20 20:45:22 +0900

    6章写経完了

--- src/ch06/torch_hindsight_experience_replay_dqn.py
+++ src/ch06/torch_hindsight_experience_replay_dqn.py
@@ -40,7 +40,7 @@ class ReplayBuffer:
 
     def sample(self, batch_size):
         idxs = np.random.choice(len(self.buffer), batch_size)
-        samples = [slef.buffer[i] for i in idxs]
+        samples = [self.buffer[i] for i in idxs]
         states, actions, rewards, next_states, done_flags = list(zip(*samples))
         return (
             np.array(states),
@@ -50,48 +50,45 @@ class ReplayBuffer:
             np.array(done_flags),
         )
 
-    def td_loss_dqn(
-        agent,
-        target_network,
-        states,
-        actions,
-        rewards,
-        next_states,
-        done_flags,
-        gamma=0.99,
-        device=DEVICE,
-    ):
-        # convert numpy array to torch tensors
-        states = torch.tensor(np.array(states), device=DEVICE, dtype=torch.float)
-        actions = torch.tensor(np.array(actions), device=DEVICE, dtype=torch.long)
-        rewards = torch.tensor(np.array(rewards), device=DEVICE, dtype=torch.float)
-        next_states = torch.tensor(
-            np.array(next_states), device=DEVICE, dtype=torch.float
-        )
-        done_flags = torch.tensor(
-            np.array(done_flags.astype("float32"), device=DEVICE, dtype=torch.float)
-        )
 
-        # get q-values for all actions in current states use agent network
-        q_s = agent(states)
+def td_loss_dqn(
+    agent,
+    target_network,
+    states,
+    actions,
+    rewards,
+    next_states,
+    done_flags,
+    gamma=0.99,
+    device=DEVICE,
+):
+    # convert numpy array to torch tensors
+    states = torch.tensor(np.array(states), device=DEVICE, dtype=torch.float)
+    actions = torch.tensor(np.array(actions), device=DEVICE, dtype=torch.long)
+    rewards = torch.tensor(np.array(rewards), device=DEVICE, dtype=torch.float)
+    next_states = torch.tensor(np.array(next_states), device=DEVICE, dtype=torch.float)
+    done_flags = torch.tensor(np.array(done_flags.astype("float32")), device=DEVICE, dtype=torch.float)
 
-        # select q-values for chosen actions
-        q_s_a = q_s[range(len(actions)), actions]
+    # get q-values for all actions in current states use agent network
+    q_s = agent(states)
 
-        with torch.no_grad():
-            # compute q-values for all actions in next states use target network
-            q_s1 = target_network(next_states)
+    # select q-values for chosen actions
+    q_s_a = q_s[range(len(actions)), actions]
 
-            # compute Qmax(next_states, actions) using predicted next q-values
-            q_s1_a1max, _ = torch.max(q_s1, dim=1)
+    with torch.no_grad():
+        # compute q-values for all actions in next states use target network
+        q_s1 = target_network(next_states)
 
-            # compute "target q-values"
-            target_q = rewards + gamma * q_s1_a1max * (1 - done_flags)
+        # compute Qmax(next_states, actions) using predicted next q-values
+        q_s1_a1max, _ = torch.max(q_s1, dim=1)
 
-        # mean squared error loss to minimize
-        loss = torch.mean((q_s_a - target_q.detach()) ** 2)
+        # compute "target q-values"
+        target_q = rewards + gamma * q_s1_a1max * (1 - done_flags)
 
-        return loss
+    # mean squared error loss to minimize
+    loss = torch.mean((q_s_a - target_q.detach()) ** 2)
+
+    return loss
 
 
 class BitFlipEnvironment:
@@ -103,14 +100,14 @@ class BitFlipEnvironment:
 
     def reset(self):
         self.state = np.random.randint(2, size=self.bits).astype(np.float32)
-        self.goal = np.random.randint(2, size=self.bits).astype(np.flaot32)
+        self.goal = np.random.randint(2, size=self.bits).astype(np.float32)
         if np.allclose(self.state, self.goal):
             self.reset()
         return self.state.copy(), self.goal.copy()
 
     def step(self, action):
         self.state[action] = 1 - self.state[action]
-        reward, done = self.compute_reward(self.state, self.gaol)
+        reward, done = self.compute_reward(self.state, self.goal)
         return self.state.copy(), reward, done
 
     def render(self):
@@ -174,7 +171,7 @@ def train_her(
     batch_size,
     td_loss_fn,
 ):
-    success_rate = []
+    success_rate = 0.0
     success_rates = []
 
     exp_replay = ReplayBuffer(10**6)
@@ -182,13 +179,10 @@ def train_her(
     for epoch in range(num_epochs):
         # Decay epsilon linearly from eps_max to eps_min
         eps = max(
-            eps_max
-            - epoch * (eps_max - eps_min) / int(num_epochs * exploration_fraction),
+            eps_max - epoch * (eps_max - eps_min) / int(num_epochs * exploration_fraction),
             eps_min,
         )
-        print(
-            f"Epoch: {epoch + 1}, exploration: {100 * eps:.0f}%, success rate: {success_rate:.2f}"
-        )
+        print(f"Epoch: {epoch + 1}, exploration: {100 * eps: .0f}%, success rate: {success_rate: .2f}")
         agent.epsilon = eps
         target_network.epsilon = eps
 
@@ -197,7 +191,7 @@ def train_her(
             for episode in range(num_episodes):
                 # Run episode and cache trajectory
                 episode_trajectory = []
-                state, goal = env.reset()[0]
+                state, goal = env.reset()
 
                 for step in range(num_bits):
                     state_ = np.concatenate([state, goal])
@@ -216,9 +210,7 @@ def train_her(
                 for t in range(steps_taken):
                     # Usual experience replay
                     state, action, reward, next_state, done = episode_trajectory[t]
-                    state_, next_state_ = np.concatenate((state, goal)), np.concatenate(
-                        (next_state, goal)
-                    )
+                    state_, next_state_ = np.concatenate((state, goal)), np.concatenate((next_state, goal))
                     exp_replay.add(state_, action, reward, next_state_, done)
 
                     # Hindsight experience replay
@@ -226,19 +218,13 @@ def train_her(
                         future = random.randint(t, steps_taken)
                         new_goal = episode_trajectory[future][3]
                         new_reward, new_done = env.compute_reward(next_state, new_goal)
-                        state_, next_state_ = np.concatenate(
-                            (state, new_goal)
-                        ), np.concatenate((next_state, new_goal))
-                        exp_replay.add(
-                            state_, action, new_reward, next_state_, new_done
-                        )
+                        state_, next_state_ = np.concatenate((state, new_goal)), np.concatenate((next_state, new_goal))
+                        exp_replay.add(state_, action, new_reward, next_state_, new_done)
 
             # Optimize DQN
             for opt_step in range(num_opt_steps):
                 # train by sampling batch_size of data from experience replay
-                states, actions, rewards, next_states, done_flags = exp_replay.sample(
-                    batch_size
-                )
+                states, actions, rewards, next_states, done_flags = exp_replay.sample(batch_size)
                 # loss = <compute TD loss>
                 optimizer.zero_grad()
                 loss = td_loss_fn(
@@ -261,6 +247,8 @@ def train_her(
         success_rates.append(success_rate)
 
     # print graph
+    plt.plot(success_rates, label="HER-DQN")
+
     plt.legend()
     plt.xlabel("Epoch")
     plt.ylabel("Success rate")
@@ -268,3 +256,48 @@ def train_her(
     plt.show(block=False)
     plt.pause(2)
     plt.close()
+
+
+def main():
+    num_bits = 50
+    n_actions = num_bits
+    state_size = 2 * num_bits
+
+    future_k = 4
+    num_epochs = 40
+    num_cycles = 50
+    num_episodes = 16
+    num_opt_steps = 40
+    eps_max = 0.2
+    eps_min = 0.0
+    exploration_fraction = 0.5
+    batch_size = 128
+
+    env = BitFlipEnvironment(num_bits)
+
+    agent = DuelingMLP(state_size, n_actions, epsilon=1).to(DEVICE)
+    target_network = DuelingMLP(state_size, n_actions, epsilon=1).to(DEVICE)
+    target_network.load_state_dict(agent.state_dict())
+    optimizer = optim.Adam(agent.parameters(), lr=1e-3)
+
+    train_her(
+        env,
+        agent,
+        target_network,
+        optimizer,
+        num_epochs,
+        exploration_fraction,
+        eps_max,
+        eps_min,
+        num_cycles,
+        num_episodes,
+        num_bits,
+        future_k,
+        num_opt_steps,
+        batch_size,
+        td_loss_fn=td_loss_dqn,
+    )
+
+
+if __name__ == "__main__":
+    main()
